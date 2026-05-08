3d15307f4346a85c0cc6b1d2e7a084f3e490eea5
Liamyio <2419518022@qq.com>
2026-05-08 17:33:14 +0800

    ignore compiled fies

--- calculator.c
+++ calculator.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 int add(int a, int b) {
     return a + b;
 }
@@ -33,33 +36,65 @@ double nthRoot(int a, int b) {
 
 
 
-int main()
-{
-    int a,b;
-    char op;
-    double result;
-    scanf("%d %c %d",&a,&op,&b);
-    switch(op)
-    {
-        case '+':
-            result = add(a,b);
-            break;
-        case '-':
-            result = subtract(a,b);
-            break;
-        case '*':
-            result = multiply(a,b);
-            break;
-        case '/':
-            result = divide(a,b);
-            break;
-        case '^':
-            result = power(a,b);
-            break;
-        case 'r':
-            result = nthRoot(a,b);
-            break;
+typedef enum { VAL, ADD, SUB, MUL, DIV, POW, ROOT, END } TokenType;
+
+typedef struct {
+    TokenType type;
+    int value;
+} Token;
+
+Token getNextToken(const char **p) {
+    while (**p == ' ') (*p)++;
+    if (**p == '\0' || **p == '\n') return (Token){END, 0};
+    if (**p == '+') { (*p)++; return (Token){ADD, 0}; }
+    if (**p == '-') { (*p)++; return (Token){SUB, 0}; }
+    if (**p == '*') { (*p)++; return (Token){MUL, 0}; }
+    if (**p == '/') { (*p)++; return (Token){DIV, 0}; }
+    if (**p == '^') { (*p)++; return (Token){POW, 0}; }
+    if (**p == 'r') { (*p)++; return (Token){ROOT, 0}; }
+    int val = 0;
+    while (isdigit(**p)) {
+        val = val * 10 + (**p - '0');
+        (*p)++;
     }
-    printf("%lf",result);
+    return (Token){VAL, val};
+}
+
+double applyOp(double a, TokenType op, int b) {
+    switch (op) {
+        case ADD: return add((int)a, b);
+        case SUB: return subtract((int)a, b);
+        case MUL: return multiply((int)a, b);
+        case DIV: return divide((int)a, b);
+        case POW: return power((int)a, b);
+        case ROOT: return nthRoot((int)a, b);
+        default:  return a;
+    }
+}
+
+int main() {
+    char line[256];
+    fgets(line, sizeof(line), stdin);
+
+    const char *p = line;
+    Token t = getNextToken(&p);
+    if (t.type != VAL) { printf("Invalid input\n"); return 1; }
+
+    double result = t.value;
+
+    while (1) {
+        Token op = getNextToken(&p);
+        if (op.type == END) break;
+
+        Token next = getNextToken(&p);
+        if (next.type != VAL) { printf("Invalid input\n"); return 1; }
+
+        result = applyOp(result, op.type, next.value);
+    }
+
+    printf("%g\n", result);
+    system("pause");
     return 0;
+
+
 }
